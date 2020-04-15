9c7043d46b6a866d19143980d6a3517210d07a45
Max Gontijo de Oliveira <maxopala@gmail.com>
2020-04-14 21:21:20 -0300

    Soma realizada

--- main.cpp
+++ main.cpp
@@ -5,5 +5,18 @@ using namespace std;
 int main()
 {
     cout << "Calculadora Simplona" << endl;
+
+    int x;
+    int y;
+
+    cout << "X: ";
+    cin >> x;
+    cout << "Y: ";
+    cin >> y;
+
+    int s = x + y;
+
+    cout << "Soma = " << s << "\n";
+
     return 0;
 }
